06331ee3c6e78bf19225d66cf147b89e31e22f09
Kazu Yamamoto <kazu@iij.ad.jp>
2013-05-31 14:53:21 +0900

    cleaning up.

--- Main.hs
+++ Main.hs
@@ -10,12 +10,12 @@ import Unique
 main :: IO ()
 main = do
     let ks = map Unique [1..]
-    rs <- replicateM 200 entry
+    rs <- replicateM 200 randomIO
     let q = fold $ zip ks rs
     print q
     putStrLn "Before atMost"
     hFlush stdout
-    let (xs,q') = Q.atMost 0.5 q
+    let (xs,q') = Q.atMost 0.5 q -- this causes seqfault with -O2
     print xs
     print q'
     putStrLn "After atMost"
@@ -23,6 +23,3 @@ main = do
 fold :: [(Q.Key, Q.Prio)] -> Q.PSQ ()
 fold []         = Q.empty
 fold ((u,r):xs) = Q.insert u r () $ fold xs
-
-entry :: IO Q.Prio
-entry = randomIO
